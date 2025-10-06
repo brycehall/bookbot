f6e04fdd00858e60f3ac526f7a1f8bb6d129f03d
brycehall <bhall.45.bh@gmail.com>
2025-10-06 15:26:23 -0500

    updated to include get_num_char

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 
 def main():
 
     words = get_num_words("books/frankenstein.txt")
-
+    characters = get_num_char("books/frankenstein.txt")
+    
     print(f"Found {words} total words")
+    print(characters)
 
 main()
